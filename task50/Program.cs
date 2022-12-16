b596d5b7bdbe5b4d6f163a11020bff3fe97e0dc9
Nikita <nikita.serg.utkin@gmail.com>
2022-12-16 09:31:21 +0900

    работает 50

---
+++ task50/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве,
+//  и возвращает позицию этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}\t");
+        Console.WriteLine();
+    }
+}
+
+void SearchArray(int[,] array)
+{
+    Console.WriteLine("Введите целое число от 0 до 9");
+    int n = Convert.ToInt32(Console.ReadLine());
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            if (array[i, j] == n)
+                Console.WriteLine($"число {n} занимает[{i},{j}] позицию");
+        else
+                Console.WriteLine($"такого числа нет на позиции [{i},{j}]");
+}
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+SearchArray(array);
